08c2ef638131fd311f04ef3442ef7330478abf7b
matildehomem <matilde.a.s.homem@gmail.com>
2018-11-26 23:54:37 +0000

    chart js implementation

--- app.js
+++ app.js
@@ -1,94 +1,131 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const list = document.querySelector('#book-list ul');
-    const totalDiv = document.querySelector('#total');
+    var addBudget = document.forms['budget'];
+    var totalBudget = document.querySelector('#totalBudget');
+    const list = document.querySelector('#gifts-list ul');
+    const total = document.querySelector('#total');
 
-    //delete book
-    list.addEventListener('click', function (e) {
-        if (e.target.className == 'delete') {
-            const li = e.target.parentElement;
-            list.removeChild(li);
-        }
+    //chart js
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'doughnut',
+
+        // The data for our dataset
+        data: {
+            labels: ["Budget", "Total"],
+            datasets: [{
+                label: "My First dataset",
+                backgroundColor: ['#006400', '#8b0000'],
+                borderColor: ['#006400', '#8b0000'],
+                data: [0, 10],
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+
+
+    //updating chart
+
+
+    function addData() {
+        var tbudget = 0;
+        var tbudget = parseInt(totalBudget.textContent);
+        chart.data.datasets[0].data = [tbudget, 50];
+        chart.data.labels[0] = "budget";
+        chart.update();
+    }
+
+    //add budget
+    addBudget.addEventListener('submit', function (e) {
+        e.preventDefault();
+        var inputBudget = document.querySelector('input[name="budget-input"]');
+        totalBudget.textContent = inputBudget.value;
+        document.querySelector('input[name="budget-input"]').value = '';
+        addData();
     });
 
 
 
 
-    //add book
-    const addBook = document.forms['add-book'];
-    addBook.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const value = addBook.querySelector('input[type="text"]').value;
-        const priceValue = document.querySelector('input[type="number"]').value;
 
-        function totalValue() {
-            var itemCount = document.getElementsByClassName('price').length;
-            var totalValue = 0;
-            for(var i = 0; i < itemCount; i++)
-            {
-                totalValue = totalValue + priceValue;
-            }
-        };
-        totalValue();
+    //add gift
+    const addGift = document.forms['add-gift'];
+    addGift.addEventListener('submit', function (e) {
+        e.preventDefault();
+        var value = addGift.querySelector('input[type="text"]');
+        var priceValue = addGift.querySelector('input[type="number"]').value;
 
 
         //create elements
         const li = document.createElement('li');
-        const bookName = document.createElement('span');
+        const giftName = document.createElement('span');
         const deleteBtn = document.createElement('span');
         const price = document.createElement('span');
-        const totalBudget = document.createElement('p');
 
 
         //add content
         deleteBtn.textContent = 'delete';
-        bookName.textContent = value;
+        giftName.textContent = value.value;
         price.textContent = priceValue;
-        totalBudget.textContent = totalValue;
+
 
 
         //add styles
         deleteBtn.classList.add('delete');
-        bookName.classList.add('name');
+        giftName.classList.add('name');
         price.classList.add('price');
-        totalBudget.classList.add('price');
+        price.classList.add('price-value');
+        total.classList.add('total')
 
         //append to DOM 
-        li.appendChild(bookName);
+        li.appendChild(giftName);
         li.appendChild(deleteBtn);
         li.appendChild(price);
 
         //append li to DOM
         list.appendChild(li);
 
-
-        totalDiv.appendChild(totalBudget);
+        //calculate total
+        var values = document.querySelectorAll('.price-value');
+        var totalValue = 0;
+        for (var i = 0; i < values.length; i++) {
+            totalValue = totalValue + parseInt(values[i].textContent);
+        }
+        total.textContent = totalValue;
+        document.querySelector('input[type="number"]').value = "";
+        addGift.querySelector('input[type="text"]').value = "";
 
     });
 
 
-
-    //hide books
-    const hideBox = document.querySelector('#hide');
-    hideBox.addEventListener('change', function (e) {
-        if (hideBox.checked) {
-            list.style.display = 'none';
-        } else {
-            list.style.display = 'initial';
+    //delete gift
+    list.addEventListener('click', function (e) {
+        if (e.target.className == 'delete') {
+            const li = e.target.parentElement;
+            list.removeChild(li);
+            var values = document.querySelectorAll('.price-value');
+            var totalValue = 0;
+            for (var i = 0; i < values.length; i++) {
+                totalValue = totalValue + parseInt(values[i].textContent);
+            }
+            total.textContent = totalValue;
         }
     });
 
-    //filter books
-    const searchBar = document.forms['search-books'].querySelector('input');
+    //filter gifts
+    const searchBar = document.forms['search-gifts'].querySelector('input');
     searchBar.addEventListener('keyup', function (e) {
         const term = e.target.value.toLowerCase();
-        const books = list.getElementsByTagName('li');
-        Array.from(books).forEach(function (book) {
-            const title = book.firstElementChild.textContent;
+        const gifts = list.getElementsByTagName('li');
+        Array.from(gifts).forEach(function (gift) {
+            const title = gift.firstElementChild.textContent;
             if (title.toLowerCase().indexOf(term) != -1) {
-                book.style.display = 'block';
+                gift.style.display = 'block';
             } else {
-                book.style.display = 'none';
+                gift.style.display = 'none';
             }
         });
     });
@@ -107,6 +144,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             })
         }
-    })
+    });
 
-})
+
+
+
+
+
+
+
+
+
+
+});
